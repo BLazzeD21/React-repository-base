793c24211f2c3362958bde50e38891be354b5daf
Alexandr Sekerin <blazzed21@gmail.com>
2024-01-14 01:37:03 +0300

    feat: make a skeleton for the application

---
+++ src/app.jsx
@@ -0,0 +1,14 @@
+import React, { StrictMode } from 'react';
+import Main from './components/Main.jsx';
+
+import './styles/styles.scss';
+
+const App = () => {
+  return (
+    <StrictMode>
+      <Main />
+    </StrictMode>
+  );
+};
+
+export default App;
